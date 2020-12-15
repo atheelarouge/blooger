8ea558fa28df5cbf56d03ab801da96f82818fd6a
Tuncay M. Aliev <tuncaymaliev@gmail.com>
2020-12-15 20:19:48 +0300

    third commit

--- js/main.js
+++ js/main.js
@@ -13,6 +13,9 @@ $(document).ready(function() {
     loop: true,
     autoplay: true,
     autoplayTimeout: 2000,
+    dots: false,
+    nav: true,
+    navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
   });
 
 });
