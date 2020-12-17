65e414a340afbd0d3270c1641a12654adb3fa3e5
Tuncay M. Aliev <tuncaymaliev@gmail.com>
2020-12-17 20:50:32 +0300

    seven

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+const responsive = {
+  320: {
+    items: 1
+  }
+}
+
 $(document).ready(function() {
   
   $nav = $('.nav');
@@ -15,7 +21,15 @@ $(document).ready(function() {
     autoplayTimeout: 2000,
     dots: false,
     nav: true,
-    navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
+    navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+    responsive: responsive
+  });
+
+  // click to scroll top
+  $('.move-up span').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1000)
   });
 
 });
